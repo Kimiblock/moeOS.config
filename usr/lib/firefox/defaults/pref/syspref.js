c3433aa38ab937376b6ec9fd5d7a042c75c3a041
Kimiblock Moe <kimiblock@icloud.com>
2025-06-27 20:18:09 +0800

    Initial move to syspref.js

--- usr/lib/firefox/defaults/pref/syspref.js
+++ usr/lib/firefox/defaults/pref/syspref.js
@@ -1,3 +1,5 @@
 // moeOS new pref
 
-pref("general.autoconfig.testpref", true, locked)
+pref("general.autoconfig.testpref", true, locked);
+pref("widget.gtk.global-menu.wayland.enabled", true, locked);
+pref("widget.gtk.global-menu.enabled", true);
