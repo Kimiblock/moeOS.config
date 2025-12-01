382ebe251b03bb934eb7397438b03f9347390454
Kimiblock Moe <kimiblock@icloud.com>
2025-12-01 16:24:46 +0800

    firefox pref

--- usr/lib/zen-browser/defaults/pref/syspref.js
+++ usr/lib/zen-browser/defaults/pref/syspref.js
@@ -265,8 +265,8 @@ pref("media.hardware-video-decoding.force-enabled", true, locked);
 pref("widget.gtk.global-menu.enabled", true, locked);
 pref("widget.gtk.global-menu.wayland.enabled", true, locked);
 
-// This BREAKS pixel caching for scrolling
-//pref("widget.gtk.rounded-bottom-corners.enabled", false, locked);
+// This BREAKS pixel caching for scrolling || apparently not so much performance lost
+pref("widget.gtk.rounded-bottom-corners.enabled", true, locked);
 
 pref("zen.theme.content-element-separation", 0);
 pref("widget.wayland.fractional-scale.enabled", true);
